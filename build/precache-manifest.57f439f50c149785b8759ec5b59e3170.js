82ed00ce64c1af920c4c8a864e47971364f6a2b8
Yuan Li <tinaly1205@gmail.com>
2019-01-20 19:56:52 -0500

    adding files

---
+++ build/precache-manifest.57f439f50c149785b8759ec5b59e3170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51055412a31e28754036",
+    "url": "/static/js/main.51055412.chunk.js"
+  },
+  {
+    "revision": "4b7c562bbae112760c09",
+    "url": "/static/js/1.4b7c562b.chunk.js"
+  },
+  {
+    "revision": "51055412a31e28754036",
+    "url": "/static/css/main.014e233d.chunk.css"
+  },
+  {
+    "revision": "4b7c562bbae112760c09",
+    "url": "/static/css/1.10cbcbf9.chunk.css"
+  },
+  {
+    "revision": "7d46de046be4d337b1a870e83370678e",
+    "url": "/index.html"
+  }
+];
